14dfadc34ca4bce1d2820ddda1cba73235c3dc8e
Артём Максимович Полянский <Lazemir@yandex.ru>
2019-11-21 02:00:45 +0300

    Добавлена возможность использовать посчитанные переменные в дальнейших расчетах.

--- lab/lab.py
+++ lab/lab.py
@@ -7,26 +7,30 @@ sheet = wb['Vars']
 var_row_index = 2
 quant_of_calc = 0
 
+class Variable:
+    def __init__(self, name: str, value: float, abs_error: float):
+        self.name = symbols(name)
+        self.value = S(value)
+        self.abs_error = S(abs_error)
+        self.rel_error = self.abs_error / self.value
+
+calculated_vars = []
+
 while sheet.cell(row = var_row_index, column = 1).value:
-    vars = []
-    values = []
-    errors = []
+    vars = calculated_vars.copy()
 
     if sheet.cell(row = var_row_index, column = 1).value[:3].lower() == 'new':
-        res_name = sheet.cell(row = var_row_index, column = 1).value[4:]
+        name = sheet.cell(row = var_row_index, column = 1).value[4:]
         quant_of_calc += 1
         formula_row_index = var_row_index
         var_row_index += 1
-        continue
+    else:
+        while sheet.cell(row = var_row_index, column = 1).value and sheet.cell(row = var_row_index, column = 1).value[:3].lower() != 'new':
+            var = Variable(sheet.cell(row = var_row_index, column = 1).value, sheet.cell(row = var_row_index, column = 2).value,
+                           sheet.cell(row = var_row_index, column = 3).value)
+            vars.append(var)
+            var_row_index += 1
 
-    while sheet.cell(row = var_row_index, column = 1).value and sheet.cell(row = var_row_index, column = 1).value[:3].lower() != 'new':
-        var = sheet.cell(row = var_row_index, column = 1).value
-        vars.append(var)
-        values.append(sheet.cell(row = var_row_index, column = 2).value)
-        errors.append(sheet.cell(row = var_row_index, column = 3).value)
-        var_row_index += 1
-
-    sym = []
     sum_square_errors = 0
 
     if sheet.cell(row = formula_row_index, column = 4).value:
@@ -34,32 +38,37 @@ while sheet.cell(row = var_row_index, column = 1).value:
     else:
         formula = S(sheet.cell(row=2, column=5).value)
 
-    for i in range(len(vars)):
-        sym.append(symbols(vars[i]))
+    for var in vars:
+        der = diff(formula, var.name)
+        sq_err = der**2 * var.abs_error**2
+        sum_square_errors += sq_err
 
-    for i in range(len(vars)):
-        der = diff(formula, sym[i])
-        err = der**2 * errors[i]**2
-        sum_square_errors += err
+    for var in vars:
+        formula = formula.subs(var.name, var.value)
+        sum_square_errors = sum_square_errors.subs(var.name, var.value)
 
-    for i in range(len(vars)):
-        formula = formula.subs(sym[i], values[i])
-        sum_square_errors = sum_square_errors.subs(sym[i], values[i])
+    try:
+        res = Variable(name, float(formula), sum_square_errors ** 0.5)
+    except TypeError:
+        error = True
+    else:
+        error = False
 
-    error = sum_square_errors ** 0.5
+    if not error:
+        calculated_vars.append(res)
 
-    sheet.cell(row= quant_of_calc + 1, column=7).value = res_name
-    sheet.cell(row= quant_of_calc + 1, column=8).value = float(formula)
-    sheet.cell(row= quant_of_calc + 1, column=9).value = float(error)
-    sheet.cell(row= quant_of_calc + 1, column=10).value = float(error / formula)
+        sheet.cell(row= quant_of_calc + 1, column=7).value = str(res.name)
+        sheet.cell(row= quant_of_calc + 1, column=8).value = float(res.value)
+        sheet.cell(row= quant_of_calc + 1, column=9).value = float(res.abs_error)
+        sheet.cell(row= quant_of_calc + 1, column=10).value = float(res.rel_error)
 
-    wb.save('vars.xlsx')
+        print('Название:', str(res.name))
+        print('Значение:', float(res.value))
+        print('Абсолютная погрешность:', float(res.abs_error))
+        print('Относительная погрешность: ', float(res.rel_error * 100), '%')
+        print()
 
-    print('Название:', res_name)
-    print('Значение:', float(formula))
-    print('Абсолютная погрешность:', float(error))
-    print('Относительная погрешность: ', float(error / formula * 100), '%')
-    print()
 
+wb.save('vars.xlsx')
 
 
